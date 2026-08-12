f83dbe6dc64a20aaca7660b10f8fe47e3fc5781c
munal <emailaddress9810@gmail.com>
2026-08-12 13:22:22 +0545

    Register serializer and password hashing

--- backend/apps/accounts/serializers.py
+++ backend/apps/accounts/serializers.py
@@ -1,13 +1,13 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only = True , min_length= 8)
-    
+    password = serializers.CharField(write_only=True, min_length=8)
+
     class Meta:
         model = User
-        fields = ["username","emmail","password"]
-        
-        def create(self, validated_data):
-            return User.objects.create_user(**validated_data)
-    
+        fields = ["username", "email", "password"]
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
