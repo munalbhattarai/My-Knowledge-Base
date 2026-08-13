10ada63dd7dd1f758c6cae5aefa5be00945a2092
munal <emailaddress9810@gmail.com>
2026-08-13 13:32:10 +0545

    added tag model in notes app

--- backend/apps/notes/models.py
+++ backend/apps/notes/models.py
@@ -7,11 +7,18 @@ class Category(models.Model):
     
     def __str__(self):
         return self.name
-
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=100 , unique=True)
+    
+    def __str__(self):
+        return self.name
+        
 
 class Note(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
     category = models.ForeignKey(Category , on_delete=models.SET_NULL, null=True ,blank=True,   related_name="notes")
+    tags = models.ManyToManyField(Tag, blank=True, related_name="notes")
     
     title = models.CharField(max_length=225)
     content = models.TextField()
