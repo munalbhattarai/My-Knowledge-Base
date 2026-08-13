ad41ab14565d57821c1840970870c260cd9e542f
munal <emailaddress9810@gmail.com>
2026-08-13 13:09:16 +0545

    added category model in notes app

--- backend/apps/notes/models.py
+++ backend/apps/notes/models.py
@@ -2,9 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    
+    def __str__(self):
+        return self.name
+
 
 class Note(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+    category = models.ForeignKey(Category , on_delete=models.SET_NULL, null=True ,blank=True,   related_name="notes")
     
     title = models.CharField(max_length=225)
     content = models.TextField()
