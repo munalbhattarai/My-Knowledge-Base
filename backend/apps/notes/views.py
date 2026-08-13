9a0a5132836a85d523be36fd05aa447daf97ca08
munal <emailaddress9810@gmail.com>
2026-08-13 12:21:17 +0545

    updated the notes views

--- backend/apps/notes/views.py
+++ backend/apps/notes/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Note
+from .serializer import NoteSerializer
 
 # Create your views here.
+
+class NoteViewSet(viewsets.ModelViewSet):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return Note.objects.filter(owner = self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(owner= self.request.user) 
+        
