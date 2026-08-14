5d105eed2d425cac7c5e15eae107bcd2012fe7b1
munal <emailaddress9810@gmail.com>
2026-08-14 21:22:16 +0545

    url for the resource

--- backend/apps/notes/urls.py
+++ backend/apps/notes/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
-from .views import NoteViewSet
+from .views import NoteViewSet, ResourceViewSet
 
 router = DefaultRouter()
 router.register("notes", NoteViewSet, basename="note")
+router.register("resources", ResourceViewSet, basename="resource")
 
 urlpatterns = router.urls
