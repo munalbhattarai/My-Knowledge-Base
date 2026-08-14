0973abea707fc1b4e720fff53c9874add4e3d631
munal <emailaddress9810@gmail.com>
2026-08-14 21:19:29 +0545

    added the resource view in notes app

--- backend/apps/notes/views.py
+++ backend/apps/notes/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
-from .models import Note
-from .serializer import NoteSerializer
+from .models import Note, Resource
+from .serializer import NoteSerializer, ResourceSerializer
+from rest_framework.exceptions import ValidationError
 
 # Create your views here.
 
@@ -16,4 +17,22 @@ class NoteViewSet(viewsets.ModelViewSet):
     
     def perform_create(self, serializer):
         serializer.save(owner= self.request.user) 
+
+class ResourceViewSet(viewsets.ModelViewSet):
+    serializer_class = ResourceSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return Resource.objects.filter(
+            note__owner = self.request.user
+        )
+        
+    def perform_create(self, serializer):
+        note = serializer.validated_data["note"]
+        
+        if note.owner != self.request.user:
+            raise ValidationError(
+                "You cannot add the resources to another person notes"
+            )
+        serializer.save()
         
